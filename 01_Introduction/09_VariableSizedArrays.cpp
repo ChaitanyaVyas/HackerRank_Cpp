27cd4f5f55a6fa3d27e8dd32b892ea6bbd11efd2
MLauper <laupm4@bfh.ch>
2016-03-21 21:46:21 +0100

    Added Variable Sized Arrays

---
+++ 01_Introduction/09_VariableSizedArrays.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int num_of_sequences, num_of_queries;
+
+    scanf("%d %d", &num_of_sequences, &num_of_queries);
+
+    int** sequences[num_of_sequences];
+
+    for (int i = 0; i < num_of_sequences; i++){
+        int num_of_elements_in_sequence;
+        scanf("%d", &num_of_elements_in_sequence);
+        int* current_sequence[num_of_elements_in_sequence];
+        sequences[i] = current_sequence;
+        for (int j = 0; j < num_of_elements_in_sequence; j++){
+            cin>>(*sequences[i])[j];
+        }
+    }
+
+    for (int i = 0; i < num_of_queries; i++){
+        int sequence, element;
+        scanf("%d %d", &sequence, &element);
+        printf("%d\n", *sequences[sequence][element]);
+    }
+
+    return 0;
+}
